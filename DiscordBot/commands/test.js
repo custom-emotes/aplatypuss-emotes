eecd7d9b7a35798e3979b3249a980a65f5366512
jdude700 <info@jdude.tv>
2023-04-11 17:49:58 +1000

    pushing all files to git

--- DiscordBot/commands/test.js
+++ DiscordBot/commands/test.js
@@ -1,6 +1,16 @@
 const { SlashCommandBuilder } = require('discord.js');
-const childProcess = require("child_process");
-const git = childProcess.exec("git pull");
+const { exec } = require('child_process');
+let directory = "../root/badges.json";
+const fs = require("fs");
+let info = "";
+
+// fs.writeFile(directory, info, (err) => {
+// 	if (err) throw err;
+// 	console.log('Data written to file');
+// });
+
+let text = Date.now().toString();
+console.log(text);
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -12,10 +22,31 @@ module.exports = {
 			),
 	async execute(interaction) {
 		const input = interaction.options.getString("input");
-		git.stdout.on("data", data =>{
-			console.log(`git replied ${data}`);
-		})
-		console.log(git);
-		await interaction.reply(input);
+		exec('git pull', (err, stdout, stderr) => {
+			console.log("<<<" + stderr + ">>>");
+			 if(stdout.includes("Already up to date.")){
+				console.log(info);
+				fs.readFile(directory, 'utf8', (err, data) => {
+					if (err) throw err;
+					info = JSON.parse(data);
+					info[0].users.push(input);
+					console.log(info[0].users);
+					fs.writeFile(directory, JSON.stringify(info), (err) => {
+						if (err) throw err;
+						console.log('Data written to file');
+						exec(`git add --all`, (err, stdout, stderr) => {
+							console.log(err)
+							exec(`git commit -m "pushing all files to git`, (err, stdout, stderr) => {
+								console.log(err)
+								exec(`git push`, (err, stdout, stderr) => {
+									console.log(err)
+								});
+							});
+						});
+					});
+				  });
+			 }
+		   });
+		// await interaction.reply(input);
 	},
 };
